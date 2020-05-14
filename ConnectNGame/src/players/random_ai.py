a1f1d4690ff58b4f72d0e4d7a809f2050f797e13
mr008 <976728139@qq.com>
2020-05-14 15:17:54 -0700

    Get started

---
+++ ConnectNGame/src/players/random_ai.py
@@ -0,0 +1,2 @@
+class RandomAi(...):
+    VISIBLE_CHARACTERS = [chr(i) for i in range(ord('!'), ord('~') + 1)]
---
+++ main.py
@@ -0,0 +1,10 @@
+def main() -> None:
+    """
+    Run the program
+    :return:
+    """
+
+
+if __name__ == '__main__':
+    main()
+
