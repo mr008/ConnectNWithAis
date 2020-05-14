20bb810e68f1b4ce43936be11f5a225bd4c7851d
acmoats <acmoats@ucdavis.edu>
2020-05-14 15:35:04 -0700

    copy main class

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
+import sys
+from ConnectNGame.src.game import Game
+
+
 def main() -> None:
     """
     Run the program
     :return:
     """
+    if len(sys.argv) == 2:
+        connectn = Game.create_game_from_file(sys.argv[1])
+        connectn.play()
+    elif len(sys.argv) < 2:
+        print('Not enough command line arguments')
+        print('Usage: python3 main.py path_to_config_file')
+    else:
+        print('Too many command line arguments ')
+        print('Usage: python3 main.py path_to_config_file')
 
 
 if __name__ == '__main__':
