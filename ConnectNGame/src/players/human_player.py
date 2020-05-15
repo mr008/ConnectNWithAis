61f433ca779e14df994dd6bdd08704732394552e
mr008 <976728139@qq.com>
2020-05-14 21:08:46 -0700

    Merge branch 'master' of https://github.com/mr008/ConnectNWithAis
    # Conflicts:
    #	ConnectNGame/src/game.py
    #	ConnectNGame/src/move.py
    #	ConnectNGame/src/players/player.py

--- ConnectNGame/src/players/human_player.py
+++ ConnectNGame/src/players/human_player.py
@@ -5,4 +5,18 @@ class HumanPlayer(Player):
     def __init__(self,name: object, piece: object):
         super().__init__(name, piece)
 
+    def create_from_user_input(players: List["Player"], blank_char: str) -> "Player":
+        """
+        Create player for user input
+        :param players: The other players in the game
+        :param blank_char: The blank character in the board
+        :return: A player created from this user's input
+        """
+        while True:
+            try:
+                name = Player.get_valid_name(players)
+                piece = Player.get_valid_piece(players, blank_char)
+                return Player(name, piece)
+            except ValueError as error:
+                print(error)
 
