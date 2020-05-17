01db4d0aa6376079e509f7f780f2e59b629f299a
mr008 <976728139@qq.com>
2020-05-16 21:37:42 -0700

    simpon 1.2

--- ConnectNGame/src/players/simple_ai.py
+++ ConnectNGame/src/players/simple_ai.py
@@ -22,16 +22,24 @@ class SimpleAI(RandomAI):
         piece = SimpleAI.get_valid_piece(players, blank_char)
         return SimpleAI(name, piece)
 
-    def move(self,board,game):
+    def get_move(self,board,game):
+        ai_piece=
         for col in range(board.num_cols):
+            board.add_piece_to_column(ai_piece,col)
             number_pieces_column=board.get_column_pieces()
             if game.is_part_of_win(number_pieces_column+1,col) == True:
                 choice=col
+                board.sub_piece_to_column(ai_piece, col)
                 return move.Move(self,choice)
+            board.sub_piece_to_column(ai_piece, col)
+        opp_piece = self.piece
         for col in range(board.num_cols):
-            number_pieces_column=board.get_column_pieces()
-            if game.is_part_of_win(number_pieces_column+1,col) == True:
-                choice=col
-                return move.Move(self,choice)
+            board.add_piece_to_column(opp_piece, col)
+            number_pieces_column = board.get_column_pieces()
+            if game.is_part_of_win(number_pieces_column + 1, col) == True:
+                choice = col
+                board.sub_piece_to_column(opp_piece, col)
+                return move.Move(self, choice)
+            board.sub_piece_to_column(opp_piece, col)
         super().get_move()
 
