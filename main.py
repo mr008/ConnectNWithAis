b3343f557557683b2ba450b269089dc976e5fccc
acmoats <acmoats@ucdavis.edu>
2020-05-18 21:06:12 -0700

    whitespace fix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 import random
-from random import seed
+# from random import seed
 from ConnectNGame.src.game import Game
 
 
@@ -9,11 +9,11 @@ def main() -> None:
     Run the program
     :return:
     """
-    address = "E:/coding programs/ConnectNWithAis/config_files/3X3X3.txt"
-    #ran_seed=30
-    #random.seed(ran_seed)
-    #address = "/Users/audeclairemoats/PycharmProject/ConnectN/config_files/3X3X3.txt"
-    '''
+    # address = "E:/coding programs/ConnectNWithAis/config_files/3X3X3.txt"
+    # ran_seed=30
+    # random.seed(ran_seed)
+    # address = "/Users/audeclairemoats/PycharmProject/ConnectN/config_files/3X3X3.txt"
+
     if len(sys.argv) == 2:
         address = sys.argv[1]
         connectn = Game.create_game_from_file(address)
@@ -31,9 +31,9 @@ def main() -> None:
     else:
         print('Too many command line arguments ')
         print('Usage: python3 main.py path_to_config_file')
-    '''
     connectn = Game.create_game_from_file(address)
     connectn.play()
+
 
 if __name__ == '__main__':
     main()
