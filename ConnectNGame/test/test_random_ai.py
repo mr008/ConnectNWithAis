079f8739d38b3836c898e6d8ff0186132b44ed9b
acmoats <acmoats@ucdavis.edu>
2020-05-19 22:54:27 -0700

    reformat

--- ConnectNGame/test/test_random_ai.py
+++ ConnectNGame/test/test_random_ai.py
@@ -8,6 +8,7 @@ from ConnectNGame.src.players.random_ai import RandomAI
 from ConnectNGame.src.players.human_player import HumanPlayer
 import random
 
+
 class TestRandomAI(unittest.TestCase):
 
     def test_get_name(self):
@@ -15,11 +16,12 @@ class TestRandomAI(unittest.TestCase):
         self.assertEqual(name, "RandomAi 1")
 
     def test_get_valid_piece(self):
-        my_players = [HumanPlayer('sam', '#'),HumanPlayer('Jess', '@')]
+        my_players = [HumanPlayer('sam', '#'), HumanPlayer('Jess', '@')]
         my_piece = RandomAI.get_valid_piece(my_players, '$')
         self.assertIsNot(my_piece, my_players[0].piece, my_players[1].piece)
-        self.assertIsNot(my_piece,'$')
-        self.assertIsNot(my_piece,' ')
+        self.assertIsNot(my_piece, '$')
+        self.assertIsNot(my_piece, ' ')
+
 
 if __name__ == '__main__':
     unittest.main()
